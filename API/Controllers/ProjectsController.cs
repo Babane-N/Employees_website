400c357e9452e4d7b2ee5d0b27d80d1ab7ec863d
Ntwisiso <amagames.br@gmail.com>
2024-05-15 18:49:48 +0200

    fii

---
+++ API/Controllers/ProjectsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.Data;
+using backend.Data.Models;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProjectsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Projects
+        // GET: api/Project/?pageIndex=0&pageSize=10&sortColumn=name&
+        // sortOrder=asc
+        [HttpGet]
+        public async Task<ActionResult<ApiResult<Project>>> GetProjects(
+            int pageIndex = 0,
+            int pageSize = 5,
+            string? sortColumn = null,
+            string? sortOrder = null,
+            string? filterColumn = null,
+            string? filterQuery = null)
+
+        {
+            return await ApiResult<Project>.CreateAsync(
+           _context.Projects.AsNoTracking(),
+              pageIndex,
+              pageSize,
+              sortColumn,
+              sortOrder,
+              filterColumn,
+              filterQuery);
+
+
+        }
+
+        // GET: api/Projects/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Project>> GetProject(int id)
+        {
+          if (_context.Projects == null)
+          {
+              return NotFound();
+          }
+            var project = await _context.Projects.FindAsync(id);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return project;
+        }
+
+        // PUT: api/Projects/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProject(int id, Project project)
+        {
+            if (id != project.ProjectId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(project).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProjectExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Projects
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Project>> PostProject(Project project)
+        {
+          if (_context.Projects == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Projects'  is null.");
+          }
+            _context.Projects.Add(project);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProject", new { id = project.ProjectId }, project);
+        }
+
+        // DELETE: api/Projects/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProject(int id)
+        {
+            if (_context.Projects == null)
+            {
+                return NotFound();
+            }
+            var project = await _context.Projects.FindAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            _context.Projects.Remove(project);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProjectExists(int id)
+        {
+            return (_context.Projects?.Any(e => e.ProjectId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ API/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using backend.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System.Diagnostics.Metrics;
+
+namespace API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base() { }
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Project> Projects => Set<Project>();
+        public DbSet<Assignment> Assignments => Set<Assignment>();
+        public DbSet<Employee> Employees => Set<Employee>();
+    }
+}
